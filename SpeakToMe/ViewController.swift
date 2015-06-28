264741b5452d967881b57f8756b86e8b098b4fdd
Mohamed El-Alfy <mo7amedelalfy@gmail.com>
2015-06-28 04:02:14 +0200

    FirstCommit

--- SpeakToMe/ViewController.swift
+++ SpeakToMe/ViewController.swift
@@ -1,15 +1,24 @@
 //
 //  ViewController.swift
-//  SpeakToMe
+//  TexttoSpeechApp
 //
-//  Created by Mohamed El-Alfy on 4/23/15.
-//  Copyright (c) 2015 Mohamed El-Alfy. All rights reserved.
+//  Created by Mohamed El-Alfy on 12/16/14.
+//  Copyright (c) 2014 Mohamed El-Alfy. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextViewDelegate {
 
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    let synth = AVSpeechSynthesizer()
+    
+    var myUtterance :AVSpeechUtterance!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textToSpeech(sender: AnyObject) {
+        myUtterance = AVSpeechUtterance(string: textView.text)
+        myUtterance.rate = 0.1
+        synth.speakUtterance(myUtterance)
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+
+    
+    func textViewShouldEndEditing(textView: UITextView) -> Bool {
+        return true
+    }
 
 }
 
